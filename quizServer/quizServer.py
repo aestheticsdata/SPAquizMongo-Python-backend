0850994b9f505de1ea1432974024fb3097429875
aestheticsdata <aesthetics.data@gmail.com>
2016-10-27 17:43:31 +0200

    quizServer.py added

---
+++ quizServer/quizServer.py
@@ -0,0 +1,53 @@
+from flask import Flask, jsonify, request, abort
+from flask.ext.pymongo import PyMongo
+from flask_cors import CORS
+
+import math
+import random
+
+
+app = Flask(__name__)
+
+app.config['MONGO_DBNAME'] = 'quiz'
+app.config['MONGO_HOST']   = '1990computer.com'
+
+CORS(app)
+
+mongo = PyMongo(app)
+
+
+
+@app.route('/login', methods=['POST'])
+def get_questions_after_login():
+
+    request_json = request.get_json()
+
+    print(request_json)
+
+    if not (request_json['username'] == 'guest' and request_json['password'] == '15032014'):
+        abort(401)
+    else:
+        questions = mongo.db.questions
+
+        output = []
+
+        for q in questions.find():
+            output.append({'question': q['question'], 'choices': q['choices'], 'correctAnswer': q['correctAnswer']})
+
+        shuffle(output)
+
+    return jsonify(result=output)
+
+
+def shuffle(a):
+    for i in range(len(a)):
+        j= math.floor(random.random()*i)
+        tmp=a[i-1]
+        a[i-1]=a[j]
+        a[j]=tmp
+
+
+
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0", port=5000, debug=True)
